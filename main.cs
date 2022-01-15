b147251d38855abb2f9192542d346d8f67655422
VoidMusics <81393246+VoidMusics@users.noreply.github.com>
2022-01-14 21:50:41 -0500

    Add files via upload

---
+++ main.cs
@@ -0,0 +1,29 @@
+.content {
+    background-color: #404040;
+    width: 940px;
+    margin: 0 auto;
+    padding: 20px 10px;
+    overflow: hidden;
+    border-bottom-left-radius: 6px;
+    border-bottom-right-radius: 6px;
+}
+
+.photo {
+    border-radius: 6px;
+    border: 1px solid #ccc;
+    width: 298px;
+    height: 298px;
+    margin-top: 10px;
+    margin-left: 10px;
+    float: left;
+    overflow: hidden;
+    position: relative;
+}
+
+.photo-name {
+    /*margin-top: 245px;*/
+    /*padding: 0.5em;*/
+    text-align: center;
+    background-color: cyan;
+    /*background-color: #666666;*/
+}
