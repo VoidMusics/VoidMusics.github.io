c2b98bdbcb86be54c028fb9bf3a5a0eeec414c7e
VoidMusics <81393246+VoidMusics@users.noreply.github.com>
2022-01-15 17:49:29 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+/*window.onload = function() {
+    alert("Eli");
+    
+}*/
+
+document.onclick= function(event) {
+    document.getElementById("myAudio").play();
+}
+
+/*window.onload = function() {
+    document.getElementById("earthquake").play();
+}*/
+
+/*var audio = document.getElementById("audio");
+audio.src = URL.createObjectURL("earthquake.mp3");
+audio.load();
+audio.play();*/
+
+/*var audio = new Audio('earthquake.mp3');
+audio.play();*/
+/*
+var scrolbefore = false;
+
+window.onscroll = function (e) {  
+    // called when the window is scrolled.  
+    if(scrolbefore) return;
+    var audio = new Audio('earthquake.mp3');
+    audio.play();
+
+    scrolbefore = true;
+    } */
